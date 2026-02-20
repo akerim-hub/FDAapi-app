bff72ce3bcbec414bee3cb2a538fd541b093e757
Syed Mohammad Qasim <sqasim@bu.edu>
2026-02-19 19:07:53 -0500

    fix code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,request, jsonify
+import requests
 
 app = Flask(__name__)
 app.users_by_id = {}
@@ -16,8 +17,6 @@ def hello_world():
 @app.route("/create_user", methods=["POST"])
 def create_user():
     data = request.get_json(silent=True) or {}
-
-    # accept either "id" or "user_id"
     user_id = data.get("id", data.get("user_id"))
     name = data.get("name")
     new_user = User(user_id=user_id, name=str(name))
@@ -26,13 +25,14 @@ def create_user():
         return jsonify({"error": f"User with id={user_id} already exists."}), 409
 
     app.users_by_id[user_id] = new_user
+    print("created user"+str(new_user))
     
-    return jsonify({"created": user.to_dict(), "total_users": len(app.users_by_id)}), 201
+    return jsonify({"created": str(new_user), "total_users": len(app.users_by_id)}), 201
 
 
 @app.route("/users", methods=["GET"])
 def list_users():
-    return jsonify([u.to_dict() for u in app.users_by_id.values()]), 200
+    return jsonify([str(u) for u in app.users_by_id.values()]), 200
 
 
 
